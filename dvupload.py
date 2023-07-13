707c5d13116555ad11751258da93969fce81c139
Andrzej Zemła <azemla@agh.edu.pl>
2023-07-13 12:56:03 +0200

    added logging

--- dvupload.py
+++ dvupload.py
@@ -3,9 +3,9 @@ from __future__ import annotations
 import hashlib
 import http
 import json
+import logging
 import mimetypes
 import os
-import pprint
 
 import requests
 
@@ -42,8 +42,8 @@ class DVUpload:
                                           params={'persistentId': f'doi:{doi}', 'size': file_size})
 
             if presigned_url.status_code != http.HTTPStatus.OK:
-                print (f'ERROR: Bad response from server, status code: {presigned_url.status_code}')
-                pprint.pprint(presigned_url.json())
+                logging.error(f'Bad response from server, status code: {presigned_url.status_code}')
+                logging.error(f'{presigned_url.json()}')
                 return file_metadata
 
             file_metadata["data"]["storageIdentifier"] = presigned_url.json ()["data"]["storageIdentifier"]
@@ -55,25 +55,25 @@ class DVUpload:
             else:
                 return file_metadata
 
-        except requests.exceptions.ConnectionError as cerr:
-            print (
-                f'ERROR: ConnectionError: server_url: {self.server_url}, API_TOKEN: {self.API_TOKEN}\nTraceback: {cerr.with_traceback ()}')
+        except requests.exceptions.ConnectionError as ce:
+            logging.exception (
+                f'ConnectionError: server_url: {self.server_url}, API_TOKEN: {self.API_TOKEN}'
+                f'\nTraceback: {ce.with_traceback ()}')
             return file_metadata
 
         link_file = requests.post (url=f'{self.server_url}/api/datasets/:persistentId/add',
                                    headers={'X-Dataverse-key': self.API_TOKEN},
                                    params={'persistentId': f'doi:{doi}'},
-                                   files={'jsonData': (None, json.dumps (
-                                       file_metadata["data"]))})  # this is what it shoudl return, file metadata
+                                   files={'jsonData': (None, json.dumps (file_metadata["data"]))})
         if link_file.status_code == http.HTTPStatus.OK:
             file_metadata["status"] = "OK"
-            print (f'INFO: File uploaded successfully')
+            logging.debug(f'File uploaded successfully')
             return file_metadata
 
         return file_metadata
 
     def __upload_multipart(self, filename: str | os.PathLike[str], presigned_data: dict):
-        print('DEBUG: running multipart')
+        logging.debug('Starting multipart upload')
         try:
             s3_bucket_urls = presigned_data["urls"]
             part_size = presigned_data["partSize"]
@@ -84,7 +84,7 @@ class DVUpload:
                 for part_id, supplied_url in s3_bucket_urls.items ():
                     chunk = f.read (part_size)
                     md5sum.update (chunk)
-                    print (f'INFO: uploading part {part_id}')
+                    logging.debug (f'Uploading part {part_id}/{len(s3_bucket_urls)}')
                     upload_request = requests.put (url=supplied_url, headers={'x-amz-tagging': 'dv-state=temp'},
                                                    data=chunk)
                     e_tags[part_id] = upload_request.headers['ETag'].replace ('\"', '')
@@ -93,13 +93,11 @@ class DVUpload:
                                                headers={'X-Dataverse-key': self.API_TOKEN},
                                                data=json.dumps (e_tags))
 
-            if complete_mp_upload.status_code != http.HTTPStatus.OK:
+            if not complete_mp_upload.status_code == http.HTTPStatus.OK:
                 _ = requests.delete (f'{self.server_url}{presigned_data["abort"]}')
-                print (
-                    f'ERROR: Could not complete multipart upload!\n\tStatus code: {complete_mp_upload.status_code}\n\tResponse: {complete_mp_upload.text}')
+                logging.exception(
+                    f'Could not complete multipart upload!\n\tStatus code: {complete_mp_upload.status_code}\n\tResponse: {complete_mp_upload.text}')
                 return {"status": "FAILED", "checksum": None}
-
-            print (f'INFO: updating file metadata')
 
             return {"status": "OK", "checksum": {
                 '@type': 'MD5',
@@ -107,40 +105,40 @@ class DVUpload:
             }}
 
         except KeyError:
-            print (f'ERROR: wrong response form S3 storage, please contact DV administrator')
+            logging.exception(f'Wrong response form S3 storage, please contact DV administrator')
             _ = requests.delete (f'{self.server_url}{presigned_data["abort"]}')
             return {"status": "FAILED", "checksum": None}
 
         except Exception as e:
             _ = requests.delete (f'{self.server_url}{presigned_data["abort"]}')
-            print (f'ERROR: unknown error, this should never happen {e.with_traceback()}')
+            logging.exception(f'Unknown error, this should never happen {e.with_traceback()}')
 
     def __upload(self, filename: str | os.PathLike[str], presigned_data: dict):
-        print("DEBUG: running one part upload")
+        logging.debug("Running one part upload")
         if presigned_data.get ("urls") is not None:
-            print("DEBUG: this file is to big, running multipart upload")
+            logging.debug("This file is to big, running multipart upload")
             return self.__upload_multipart (filename=filename, presigned_data=presigned_data)
 
         try:
             s3_bucket_urls = presigned_data["url"]
-            print("dzy tu jest babol")
             with open (filename, 'rb') as f:
                 data_file = f.read ()
                 md5sum = hashlib.md5 ()
                 md5sum.update (data_file)
                 upload_request = requests.put (url=s3_bucket_urls, headers={'x-amz-tagging': 'dv-state=temp'},
                                                data=data_file)
-                print (f'Co mi tu zwraca put jak suckess: {upload_request.status_code}')
 
-            print (f'INFO: updating file metadata')
+                if not upload_request.status_code == http.HTTPStatus.OK:
+                    return {"status": "FAILED", "checksum": None}
+
             return {"status": "OK", "checksum": {
                 '@type': 'MD5',
                 '@value': md5sum.hexdigest ()
             }}
 
         except KeyError:
-            print (f'ERROR: wrong response form S3 storage, please contact DV administrator, {upload_request.request}')
+            logging.exception(f'Wrong response form S3 storage, please contact DV administrator, {upload_request.request}')
             return {"status": "FAILED", "checksum": None}
         except Exception as e:
-            print(f"ERROR: {e.with_traceback()}")
+            logging.exception(f"{e.with_traceback()}")
             return {"status": "FAILED", "checksum": None}
